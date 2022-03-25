5cebcf1841315de8cfebc04fc680182495165851
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-03-25 15:44:46 +0400

    install material

---
+++ reactmaterailui/src/App.js
@@ -0,0 +1,14 @@
+import AppBar from './Appbar';
+import './App.css';
+import Demo from './Demo';
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar />
+      <Demo />
+    </div>
+  );
+}
+
+export default App;
